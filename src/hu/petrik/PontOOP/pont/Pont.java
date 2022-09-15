f7f135a59c7c7c6565ea7dabaadcc54b81a1f78e
JungKata <97729333+JungKata@users.noreply.github.com>
2022-09-15 09:27:01 +0200

    Pont osztály -pont távolság az origotól

--- src/hu/petrik/PontOOP/pont/Pont.java
+++ src/hu/petrik/PontOOP/pont/Pont.java
@@ -10,13 +10,33 @@ public class Pont {
 
     }
 
+    public Pont(int n){
+        this.x = generateCoordinate(n);
+        this.y = generateCoordinate(n);
+    }
+
+    private static int generateCoordinate(int n) {
+        return (int) (Math.random() * ((2 * n) + 1)) - n;
+    }
     public Pont(int x, int y){
         this.x = x;
         this.y = y;
+    }
+
+    public double getOrigitolMertTavolsag()
+    {
+        //négyzetgyök - sQuarterrel
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
     }
 
     @Override
     public String toString() {
         return String.format("(%d, %d)", this.x, this.y);
     }
+
+
+
+
+
+
 }
