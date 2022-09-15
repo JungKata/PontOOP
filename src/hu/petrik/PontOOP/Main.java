5efc13650c1ea77c946d73c749fb73ec6a8b97d1
JungKata <97729333+JungKata@users.noreply.github.com>
2022-09-15 09:01:01 +0200

    Pont osztály -adattagok -konstruktorok -meghívás

--- src/hu/petrik/PontOOP/Main.java
+++ src/hu/petrik/PontOOP/Main.java
@@ -1,7 +1,14 @@
 package hu.petrik.PontOOP;
 
+import hu.petrik.PontOOP.pont.Pont;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Pont p1 = new Pont();
+        Pont p2 = new Pont(0,0);
+        Pont p3 = new Pont(3, 4);
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
     }
 }
